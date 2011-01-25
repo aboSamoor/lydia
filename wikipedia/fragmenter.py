e56b844b2d58ae7742ee1270510a772839331a45
aboSamoor <rmyeid@gmail.com>
2011-01-25 04:59:12 -0500

    adding wikipedia dump parser and fragmenter

---
+++ wikipedia/fragmenter.py
@@ -0,0 +1,41 @@
+from parser import subTree
+import xml.sax
+import sys
+import os
+import re
+
+outputPath = ""
+
+def op2(text):
+    print re.findall(r'<title>(.*?)</title>',text)[0].encode("utf-8")
+
+#parser2 = xml.sax.make_parser()
+#contentHandler = subTree("title", op2)
+#parser2.setContentHandler(contentHandler)
+
+def generateNewXML(text):
+    text = '<?xml version="1.0" encoding="utf-8"?>'+ text 
+    text = text.encode("utf-8")
+    m = re.search('<id>(.*?)</id>', text)
+    if m:
+        fName = outputPath+"/"+m.group(1)
+        print fName 
+        open(fName,'w').write(text)
+#    xml.sax.parseString(text, contentHandler)
+#    tmpFile ="/tmp/_987654321.xml"
+#    tmpHandler = open(tmpFile,'w')
+#    tmpHandler.write(text2.encode("utf-8"))
+#    tmpHandler.close()
+#    tmpHandler2 = open(tmpFile, 'r')
+#    parser2.parse(tmpHandler2)
+#    tmpHandler2.close()
+    
+def puts(text):
+    print text
+
+
+if __name__=="__main__":
+    outputPath = sys.argv[2]
+    parser = xml.sax.make_parser()
+    parser.setContentHandler(subTree("page", generateNewXML))
+    parser.parse(open(sys.argv[1],"r"))
---
+++ wikipedia/parser.py
@@ -0,0 +1,48 @@
+import xml.sax
+from xml.sax.saxutils import escape
+
+class subTree(xml.sax.ContentHandler):
+    def __init__(self, tag, op=None):
+        self.tag = tag
+        self.inside = False
+        self.text = ""
+        self.counter = 0
+        if op:
+            self.operation = op
+        else:
+            self.operation = self.dummy_op
+
+    def startElement(self, name, attrs):
+        self.last_opentag = name  # think about <id><id></id></id>
+        if name == self.tag:
+            self.inside = True 
+        if self.inside:
+            self.text += "<"+name
+            if attrs.keys():
+                self.text += ' '
+            for i,j in attrs.items():
+                self.text += i + '='+'"'+j+'"'
+            self.text += '>'
+            self.counter = len(self.text)
+        else:
+            self.text = ""
+
+    def endElement(self, name):
+        if self.inside:
+            if len(self.text) > self.counter: # something has been added
+                self.text = self.text[:self.counter] + escape(self.text[self.counter:])
+            self.text += "</"+name+">"
+            self.counter = len(self.text)
+        else:
+            self.text = ""
+        if name == self.tag:
+            self.operation(self.text)
+            self.inside = False
+            self.text = ""
+
+    def dummy_op(self, text):
+        pass
+    
+    def characters(self, content):
+        self.text += content
+        
