07a6b9760f7b700b645f955dd10c69bc69929056
Md Ariful Islam <arifulislam@csebuet.org>
2011-02-03 13:00:22 -0500

    adding new columns and newline after .

--- aner/splitStatements.py
+++ aner/splitStatements.py
@@ -1,11 +1,25 @@
+#! /usr/bin/env python
+
 import NER
 import sys
 import os
 import re
 
-if __name__=="__main__":
-    fin = os.path.abspath(sys.argv[1])
-    fout = os.path.abspath(sys.argv[2])
+
+def fixCols(fin, fout):
+    fin = os.path.abspath(fin)
+    fout = os.path.abspath(fout)
     text = NER.getText(fin)
-    txt = re.sub(r"([.|?|!]\tO)",'\\1\n',text)
+    txt = re.sub(r"(\nO)",'\n.\tO',text)
     NER.writeText(fout, txt)
+
+def addNewlines(fin, fout):
+    fin = os.path.abspath(fin)
+    fout = os.path.abspath(fout)
+    text = NER.getText(fin)
+    txt = re.sub(r"([.|?|!]\s*O)",'\\1\n',text)
+    NER.writeText(fout, txt)
+
+if __name__=="__main__":
+    fixCols(sys.argv[1], sys.argv[2])
+    addNewlines(sys.argv[2],sys.argv[2]+".out")
