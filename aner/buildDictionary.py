c5939f5f99b17a82257ecdf7228c1b75f4e9c2f3
aboSamoor <rmyeid@gmail.com>
2011-02-18 00:01:30 -0500

    fixing the tag bug, adding the proper nouns withou Al beside the whole names

--- aner/buildDictionary.py
+++ aner/buildDictionary.py
@@ -5,13 +5,22 @@ import os
 import sys
 import pickle
 
+# We are adding only the first part of the name, for example, red sea, we are considering only sea. This works fine with persons name but not with others ;)
 def add(dic, txt, tag):
     for line in txt.splitlines():
         words = line.split(' ')
-        if not dic.has_key(words[0]):
-            dic[words[0]] = set([tag])
-        else:
-            dic[words[0]].add(tag)
+        new = [' '.join(words), words[0]]
+        if len(words) > 1:
+            new.append(words[1])
+        for word in new:
+            if word[:2] == 'Al':
+                new.append(word[2:])
+
+        for word in new:
+            if not dic.has_key(words[0]):
+                dic[words[0]] = set([tag])
+            else:
+                dic[words[0]].add(tag)
             
 
 if __name__ == "__main__":
@@ -20,7 +29,7 @@ if __name__ == "__main__":
         sys.exit()
     gazetteer = os.path.abspath(sys.argv[1])
     data = os.path.abspath(sys.argv[2])
-    tag = os.path.abspath(sys.argv[3])
+    tag = sys.argv[3]
 
     if not os.path.isfile(gazetteer):
         dic = {}
