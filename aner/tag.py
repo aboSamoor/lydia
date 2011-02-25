3c4e3e90c8a5ae613b4fcf02b55a921e14b8775b
aboSamoor <rmyeid@gmail.com>
2011-02-25 00:37:11 -0500

    change input files to json format

--- aner/tag.py
+++ aner/tag.py
@@ -4,45 +4,43 @@ import os
 import sys
 import tools
 import pickle
+import json
 
-def tag(fName, dic):
+def isNNP(line):
+    if line["word"] != '':
+        if line["POS"][:3] == "NNP":
+            return True
+    return False
+
+def tag(jText, dic):
     tagged = []
-    txt = tools.getText(fName)
-    lines = txt.splitlines()
+    lines = jText
     for i in range(len(lines)):
-        if lines[i] != '':
-            words = lines[i].split('\t')
-            newline = words
-            newline[-1] = 'O'
-            if words[1][:3] == "NNP":
-                if not dic.has_key(words[0]):
-                    if i > 0:
-                        prevLine = tagged[-1]
-                        if len(prevLine) > 1:
-                            if len(prevLine[1]) > 2:
-                                if prevLine[1][:3] == "NNP":
-                                    newline[-1] = prevLine[-1]
-                                elif prevLine[1] == "DET":
-                                    if i > 1:
-                                        prev2Line = tagged[-2]
-                                        if len(prev2Line) > 1:
-                                            if len(prev2Line[1]) > 2:
-                                                if prev2Line[1][:3] == "NNP":
-                                                    newline[-1] = prev2Line[4]
-                else:
-                    newline[-1] = list(dic[words[0]])[0]
-            tagged.append(newline)
-        else:
-            tagged.append([''])
-    newTxt = '\n'.join(['\t'.join(l) for l in tagged])
-    tools.writeText(fName+".t",newTxt)
+        curLine = lines[i]
+        curLine["NER"] = 'O'
+        if isNNP(curLine):
+            if not dic.has_key(curLine["word"]):
+                if i > 0:
+                    if isNNP(lines[i-1]):
+                        curLine["NER"] = lines[i-1]["NER"]
+                    elif lines[i-1]["POS"] == "DET":
+                        if i > 1:
+                            if isNNP(lines[i-2]):
+                                curLine["NER"] = lines[i-2]["NER"]
+            else:
+                curLine["NER"] = list(dic[curLine["word"]])[0]
+    return jText
 
 if __name__ == "__main__":
-    folder1 = os.path.abspath(sys.argv[1])
-    dicName = os.path.abspath(sys.argv[2])
+    f = os.path.abspath(sys.argv[2])
+    dicName = os.path.abspath(sys.argv[1])
     dictionary = pickle.load(open(dicName,'r'))
-    if not os.path.isdir(folder1) or not os.path.isfile(dicName):
-        print "usage: ....." 
+    if not os.path.isfile(f):
+        print "usage: tag.py dictionary fileName ....." 
         sys.exit()
-    for fName in tools.files(folder1, "(.*?)\.NER$"):
-        tag(fName, dictionary)
+    jText = json.load(open(f, 'r'))
+    print f
+    jNewText = tag(jText, dictionary)
+    fh = open(f+".t", 'w')
+    json.dump(jNewText, fh)
+    fh.close()
