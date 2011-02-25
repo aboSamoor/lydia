04dd29e430b1251a4d1754c21e4cea1e042c4941
aboSamoor <rmyeid@gmail.com>
2011-02-25 00:33:43 -0500

    tool to change from column format to json format, needs to be fixed to deal with non utf-8 files

---
+++ aner/2json.py
@@ -0,0 +1,48 @@
+#! /usr/bin/env python
+
+import os, sys
+import tools
+import json
+
+postNERHeader = {0: "word", 1: "POS", 2:"POS1", 3:"POS2", 4:"NER"}
+
+def convert(header, f, delim):
+    fh = open(f, 'r')
+    text = fh.read()
+    try:
+        text = text.decode('utf-8')
+    except:
+        print f
+    fh.close()
+    result = []
+    for line in text.splitlines():
+        taggedWords = {}
+        words = line.split(delim)
+        if len(words) < 2:
+            for index in header.keys():
+                taggedWords[header[index]] = ''
+        else:
+            for index in header.keys():
+                taggedWords[header[index]] = words[index]
+        taggedWords["virtual"] = False
+        result.append(taggedWords)
+    return result
+
+
+if __name__=="__main__":
+    if len(sys.argv) < 2:
+        print "usage : 2json.py fileName"
+    f = os.path.abspath(sys.argv[1])
+    exts = f.split('.')
+    newFile = exts[0] + '.json' 
+    if not os.path.isfile(f):
+        print  "Error "+f +" file does not exists"
+        sys.exit()
+    if exts[-1] == "NER":
+        if exts[-2] == "post":
+            result = convert(postNERHeader, f, '\t')
+    else:
+        sys.exit()
+    fh = open(newFile, 'w')
+    json.dump(result, fh)
+    fh.close()
